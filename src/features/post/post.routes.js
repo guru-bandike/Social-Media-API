1ab98d6da96b5cc2b83536f3742adc2eadfb1c3d
guru-bandike <guru.bandike@gmail.com>
2024-05-29 13:22:03 +0530

    Add post filter route

--- src/features/post/post.routes.js
+++ src/features/post/post.routes.js
@@ -14,6 +14,7 @@ const postController = new PostController();
 // Define post routes
 postRouter.get('/', postController.getByUserId); // Route to get all user posts
 postRouter.get('/all', postController.getAll); // Route to get all existing posts
+postRouter.get('/filter', postController.filter); // Route to filter and get posts
 postRouter.get('/:id', postController.get); // Route to get a specific post
 postRouter.post('/', uploadPostMedia, logRequest, validateAddPostReq, postController.add); // Route to add new post
 postRouter.put('/:id', uploadPostMedia, logRequest, validateUpdatePostReq, postController.update); // Route to update a specific user post
