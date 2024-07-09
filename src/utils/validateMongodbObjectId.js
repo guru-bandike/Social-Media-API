618df26b793e0105003aba264979bfe23904c1a5
guru-bandike <guru.bandike@gmail.com>
2024-07-09 17:26:22 +0530

    Migrate Comment feature to MongoDB from In-Memory data structures

--- src/utils/validateMongodbObjectId.js
+++ src/utils/validateMongodbObjectId.js
@@ -3,8 +3,9 @@ import CustomError from '../errors/CustomError.js';
 
 const validateMongodbObjectId = async (id, modelName) => {
   try {
-    if (!mongoose.Types.ObjectId.isValid(id))
-      throw new CustomError(`Invalid ${modelName} Id!`, 400, { id });
+    if (!mongoose.Types.ObjectId.isValid(id) || String(id).length !== 24) {
+      throw new CustomError(`Invalid ${modelName} Id!`, 400, { [`${modelName}Id`]: id });
+    }
   } catch (err) {
     throw err;
   }
